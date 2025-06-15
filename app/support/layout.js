25ddeacae4119c91210de23b91fdab9f248b7399
abdbaap <soniaajmani6@gmail.com>
2025-06-15 18:55:47 +0530

    Update layout.js

--- app/support/layout.js
+++ app/support/layout.js
@@ -1,3 +1,4 @@
+import Navbar from "../navbar";
 export const metadata = {
   // This is the long heading that users search for and see in browser tabs for the Support page.
   title: "MCFleet Support: Need Assistance? We're Here to Help! 🤝",
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+    </Navbar>
         {children} {/* This renders the content from your page.js file */}
       </body>
     </html>
