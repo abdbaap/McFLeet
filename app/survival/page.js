f496f83267d76d3c784b6df3d4adbb15ecba1869
abdbaap <soniaajmani6@gmail.com>
2025-06-15 18:38:15 +0530

    Update page.js

--- app/survival/page.js
+++ app/survival/page.js
@@ -17,8 +17,8 @@ const Page = () => {
           visibleClass="fade-up-visible"
           duration="duration-900"
          >  <span>
-            Welcome to MCFleet's truly extensive Survival Mode! Prepare for an unparalleled Minecraft adventure with our state-of-the-art server infrastructure, specifically engineered to provide a lag-free performance. You'll experience incredibly smooth gameplay and minimal network issues, ensuring your building, exploring, and thriving are never interrupted. Our dedicated setup guarantees a stable and consistently enjoyable environment for every single player, allowing you to immerse yourself fully in the world.
-          </span></AnimateOnScroll> 
+            Welcome to MCFleet&apos;s truly extensive Survival Mode! Prepare for an unparalleled Minecraft adventure with our state&ndash;of&ndash;the&ndash;art server infrastructure, specifically engineered to provide a lag&ndash;free performance. You&apos;ll experience incredibly smooth gameplay and minimal network issues, ensuring your building, exploring, and thriving are never interrupted. Our dedicated setup guarantees a stable and consistently enjoyable environment for every single player, allowing you to immerse yourself fully in the world.
+          </span></AnimateOnScroll>
           <Image src="/survival2.jpg" width={400} height={400} alt="survival overview" className="rounded-xl"></Image>
         </div>
 
@@ -28,8 +28,8 @@ const Page = () => {
           visibleClass="fade-up-visible"
           duration="duration-900"
          >  <span>
-            Dive deep into MCFleet's dynamic and player-driven economy. Our comprehensive shop allows you to effortlessly purchase a wide array of items, enhancing your gameplay and giving you an edge in survival. When you've gathered valuable resources, you can easily sell them for in-game currency. For those looking to secure the best deals or offload rare finds, our robust auction system is perfect. Place your bids on coveted items or set up your own auctions, fostering a vibrant and engaging marketplace driven by the community.
-          </span></AnimateOnScroll> 
+            Dive deep into MCFleet&apos;s dynamic and player&ndash;driven economy. Our comprehensive shop allows you to effortlessly purchase a wide array of items, enhancing your gameplay and giving you an edge in survival. When you&apos;ve gathered valuable resources, you can easily sell them for in&ndash;game currency. For those looking to secure the best deals or offload rare finds, our robust auction system is perfect. Place your bids on coveted items or set up your own auctions, fostering a vibrant and engaging marketplace driven by the community.
+          </span></AnimateOnScroll>
           <Image src="/survival3.jpg" width={400} height={400} alt="shop and auction" className="rounded-xl"></Image>
         </div>
 
@@ -39,8 +39,8 @@ const Page = () => {
           visibleClass="fade-up-visible"
           duration="duration-900"
          >  <span>
-            Maximize your in-game efficiency with MCFleet's thoughtfully designed features. Take advantage of our dedicated AFK (Away From Keyboard) zone, a safe area where you can remain connected and productive even when you need a break from active gameplay. As you explore the vast survival world, keep an eye out for mysterious shards. These unique collectibles can be traded for valuable spawners, offering a significant boost to your resource gathering and mob farming endeavors, helping you progress faster.
-          </span></AnimateOnScroll> 
+            Maximize your in&ndash;game efficiency with MCFleet&apos;s thoughtfully designed features. Take advantage of our dedicated AFK (Away From Keyboard) zone, a safe area where you can remain connected and productive even when you need a break from active gameplay. As you explore the vast survival world, keep an eye out for mysterious shards. These unique collectibles can be traded for valuable spawners, offering a significant boost to your resource gathering and mob farming endeavors, helping you progress faster.
+          </span></AnimateOnScroll>
           <Image src="/survival4.jpg" width={400} height={400} alt="afk zone and shards" className="rounded-xl"></Image>
         </div>
 
@@ -50,8 +50,8 @@ const Page = () => {
           visibleClass="fade-up-visible"
           duration="duration-900"
          >  <span>
-            Navigating MCFleet's expansive world is incredibly easy with our comprehensive suite of teleport functions. Instantly jump to new, undiscovered areas using the /rtp command for random teleportation, perfect for finding fresh resources or scouting out new building spots. For your ultimate convenience, you can also set multiple homes at your preferred locations and teleport back to them whenever you desire, ensuring quick and efficient access to your bases, farms, or any other important points.
-          </span></AnimateOnScroll> 
+            Navigating MCFleet&apos;s expansive world is incredibly easy with our comprehensive suite of teleport functions. Instantly jump to new, undiscovered areas using the /rtp command for random teleportation, perfect for finding fresh resources or scouting out new building spots. For your ultimate convenience, you can also set multiple homes at your preferred locations and teleport back to them whenever you desire, ensuring quick and efficient access to your bases, farms, or any other important points.
+          </span></AnimateOnScroll>
           <Image src="/survival5.jpg" width={400} height={400} alt="teleport and homes" className="rounded-xl"></Image>
         </div>
 
@@ -61,8 +61,8 @@ const Page = () => {
           visibleClass="fade-up-visible"
           duration="duration-900"
          >  <span>
-            Unleash your competitive spirit and engage in thrilling combat within our dedicated PvP arena. Test your skills against other players in intense battles, proving your mastery in player-versus-player combat. You can also form powerful teams with your friends to conquer challenges together, strategize, and climb the ranks. Our dynamic leaderboards, featuring statistics on kills, team deaths, and accumulated money, update rapidly to showcase the top players and teams, fueling healthy competition.
-          </span></AnimateOnScroll> 
+            Unleash your competitive spirit and engage in thrilling combat within our dedicated PvP arena. Test your skills against other players in intense battles, proving your mastery in player&ndash;versus&ndash;player combat. You can also form powerful teams with your friends to conquer challenges together, strategize, and climb the ranks. Our dynamic leaderboards, featuring statistics on kills, team deaths, and accumulated money, update rapidly to showcase the top players and teams, fueling healthy competition.
+          </span></AnimateOnScroll>
           <Image src="/survival6.jpg" width={400} height={400} alt="pvp and leaderboards" className="rounded-xl"></Image>
         </div>
 
@@ -72,8 +72,8 @@ const Page = () => {
           visibleClass="fade-up-visible"
           duration="duration-900"
          >  <span>
-            MCFleet's Survival offers a truly holistic and rewarding experience. Here, you're encouraged to grind for valuable resources, meticulously build intricate houses and expansive bases, and earn money through a myriad of in-game activities. Beyond the peaceful aspects, you can also engage in exhilarating PvP battles, making it a well-rounded and exciting environment. It's a world where your creativity knows no bounds, where hard work is rewarded, and where every type of player can find their perfect adventure and carve out their unique legacy.
-          </span></AnimateOnScroll> 
+            MCFleet&apos;s Survival offers a truly holistic and rewarding experience. Here, you&apos;re encouraged to grind for valuable resources, meticulously build intricate houses and expansive bases, and earn money through a myriad of in&ndash;game activities. Beyond the peaceful aspects, you can also engage in exhilarating PvP battles, making it a well&ndash;rounded and exciting environment. It&apos;s a world where your creativity knows no bounds, where hard work is rewarded, and where every type of player can find their perfect adventure and carve out their unique legacy.
+          </span></AnimateOnScroll>
           <Image src="/survival1.jpg" width={400} height={400} alt="grind build earn pvp" className="rounded-xl"></Image>
         </div>
  <Link href="/"  className="text-xl bg-blue-800 text-white font-bold px-8 rounded-4xl py-4">Start Adventure Now!</Link>
