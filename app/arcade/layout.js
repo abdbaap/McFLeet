d1568870f6d5b1619470fbd99c35c0c77eeff24c
abdbaap <soniaajmani6@gmail.com>
2025-06-15 20:13:24 +0530

    Update layout.js

--- app/arcade/layout.js
+++ app/arcade/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
       <body>
         {/* This is the short heading that will be visible at the top of your webpage,
             near where you might place your icon. It's pulled from the h1 in page.js. */}
-        
+        <Navbar/>
         {children}
       </body>
     </html>
