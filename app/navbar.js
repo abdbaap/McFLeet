fd8360cfee92540c5f523e80c394a16a68317523
abdbaap <soniaajmani6@gmail.com>
2025-08-18 15:14:55 +0530

    Update navbar.js

--- app/navbar.js
+++ app/navbar.js
@@ -3,16 +3,16 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useRef } from 'react'
 
-  
+
 const Navbar = ({bg}) => {
     const ref=useRef()
   const showindex=()=>{
     ref.current.classList.toggle("hidden")
 
-      
+
     }
   return (<nav className={`flex  py-4 z-40 justify-between relative  items-center w-[100%]  ${bg}`}>
-        <Link href="#home" className="text-2xl justify-center items-center text-2xl font-extrabold gap-2 mx-2 text-white flex"><Image src={"/logo.png"} width={50} height={50} alt="mcfleet mcfleet address how to claim blocks in mcfleet mcfleeg server logo, play.mcfleet.net server mcfleet server mcfleet.net server mcfleet mcfleet minecraft server mcfleet server play.mcfleet.net mcfleet server logo ong mcfleet .net mcfleet ip store.mcfleet.net mcfleet store" className="ghg"></Image><h1> MCFleet</h1></Link>
+        <Link href="#home" className="text-2xl justify-center items-center text-2xl font-extrabold gap-2 mx-2 text-white flex"><Image src={"/logo.png"} width={50} height={50} alt="MCFleet Minecraft Server, play.mcfleet.net, store.mcfleet.net, how to claim blocks, mcfleet server logo, mcfleet ip, mcfleet.net, mcfleet" className="ghg"></Image><h1> MCFleet</h1></Link>
         <div className="dropdown group flex  flex-col mx-2">
 <svg onClick={()=>showindex()} className="w-10 invert " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
@@ -23,11 +23,11 @@ const Navbar = ({bg}) => {
               <Link href="/support" className="p-4 w-[100%] hover:bg-gray-900">Contact</Link>
               <Link href="/#blogs" className="p-4 w-[100%] hover:bg-gray-900">Blogs</Link>
             </div>
-            
+
         </div>
       </nav>
-    
-    
+
+
   )
 }
 
