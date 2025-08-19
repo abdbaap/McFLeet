4278fec2b0be627321df213e97d793e3b449b8a2
abdbaap <soniaajmani6@gmail.com>
2025-08-19 12:59:15 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -66,7 +66,7 @@ MCFleet stands as India&apos;s premier Minecraft server, meticulously crafted by
       <div id="blogs" className="flex flex-wrap gap-4 p-4 justify-center items-center text-center w-[100%]">
 {features.map((card) => {
   return <div className="card flex flex-col md:w-[35%] sm:w-[60%] w-[90%] relative p-6 gap-2 justify-center overflow-hidden items-center rounded-xl" key={card.gameMode}>
-  <Image src={card.imageUrl} objectFit="cover" alt="survival.webp"fill="true" className=" absolute w-[100%] height-[100%] top-0 -z-10 cardimg brightness-50 "></Image>
+  <Image src={card.imageUrl} objectFit="cover" alt="mcfleet mcfleet store"fill="true" className=" absolute w-[100%] height-[100%] top-0 -z-10 cardimg brightness-50 "></Image>
   <span className="text-xl text-white font-bold font-bold">
 {card.heading}</span>
   <span className="text text-white font-bold">{card.paragraph}
@@ -83,7 +83,7 @@ MCFleet stands as India&apos;s premier Minecraft server, meticulously crafted by
       </div>
 
         <div className="founder sm:flex-row flex flex-col md:flex-row justify-evenly items-center gap-4 md:p-16 p-8 my-8 w-[100%]">
-<Image src="/founder.jpeg" width={400} height={400} alt="AnshuBisht Image" className="rounded-xl "></Image><AnimateOnScroll animationClass="opacity-0 -translate-y-10" visibleClass="opacity-100 translate-y-0">
+<Image src="/founder.jpeg" width={400} height={400} alt="AnshuBisht Gamerfleet mcfleet mcfleet store" className="rounded-xl "></Image><AnimateOnScroll animationClass="opacity-0 -translate-y-10" visibleClass="opacity-100 translate-y-0">
 
         <div className="text flex flex-col justify-center items-center gap-4"><span className="text-3xl font-bold">
 Meet the Visionary: Anshu Bisht (Gamerfleet)</span>
