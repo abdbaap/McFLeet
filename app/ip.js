1c6057cde6f08b7c21b6e0a8a10992a5495f41ec
abdbaap <soniaajmani6@gmail.com>
2025-08-30 17:13:33 +0530

    Update ip.js

--- app/ip.js
+++ app/ip.js
@@ -6,7 +6,7 @@ const Ip = () => {
       <div className="flex w-[80%] flex-col justify-center items-center gap-2 p-4 my-8 bg-gray-950 text-white">
     <AnimateOnScroll animationClass="opacity-0 scale-75 " visibleClass="w-full justify-center items-center opacity-100 scale-100"
 >
-        <div className="bg-gray-950 border-b-2 flex w-[100%] justify-between border-b-black items-center font-bold text-xl ">IP: <Image src="/copy.svg" className="invert" width={50} height={50} alt="copy" onClick={()=>navigator.clipboard.writeText("play.mcfleet.net")}></Image></div>
+        <div className="bg-gray-950 border-b-2 flex w-[100%] justify-between border-b-black items-center font-bold text-xl ">Mcfleet IP: <Image src="/copy.svg" className="invert" width={50} height={50} alt="copy mcfleet mcfleet store mcfleet website mcfleetwebsite mcfleet" onClick={()=>navigator.clipboard.writeText("play.mcfleet.net")}></Image></div>
         <div className="flex justify-center items-center text-center w-[100%] p-2">play.mcfleet.net</div>
       </AnimateOnScroll>
       </div>
